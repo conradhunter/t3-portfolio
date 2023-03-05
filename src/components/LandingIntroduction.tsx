f6d0aa8deebc271a686fe9a79f03099c2cf35cb9
Conrad Hunter <conrad.hunter10@gmail.com>
2023-03-04 19:11:18 -0800

    fix char entity reference issue

--- src/components/LandingIntroduction.tsx
+++ src/components/LandingIntroduction.tsx
@@ -9,9 +9,9 @@ function LandingIntroduction() {
       </h1>
       <h2 className="text-3xl font-bold text-indigo-500">Frontend Developer</h2>
       <p className="mt-10 w-1/3 text-center text-base font-medium text-white">
-        I'm Conrad Hunter, a 24-year-old frontend developer. I take pride in
-        crafting visually stunning and easy-to-use web interfaces. With a keen
-        attention to detail and a deep understanding of user experience, I
+        I&apos;m Conrad Hunter, a 24-year-old frontend developer. I take pride
+        in crafting visually stunning and easy-to-use web interfaces. With a
+        keen attention to detail and a deep understanding of user experience, I
         deliver intuitive designs that elevate brands and delight users.
       </p>
       <SocialLinks />
