adb413691e57b719cafbac9894f144872719599c
Conrad Hunter <conrad.hunter10@gmail.com>
2023-03-04 18:47:36 -0800

    types to projects array

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,4 +1,14 @@
-export const projects = [
+interface Project {
+  id: number;
+  title: string;
+  projectDescription: string;
+  sourceCodeLink: string | null;
+  workInProgress: boolean;
+  liveLink: string;
+  projectImage: string;
+}
+
+export const projects: Project[]  = [
   {
     id: 1,
     title: "WindiKit",
@@ -15,6 +25,7 @@ export const projects = [
     projectDescription:
       "Ultraverse is a React-based mock-up NFT marketplace that provides a user-friendly platform for buying, selling, and trading NFTs. The platform is designed to showcase the features of a digital asset exchange platform and is focused on providing an intuitive and seamless user experience.",
     sourceCodeLink: "https://github.com/conradhunter/virtual-internship",
+    workInProgress: false,
     liveLink: "https://virtual-internship-ultraverse.netlify.app/",
     projectImage: require("../assets/ultraverse-ss.png"),
   },
@@ -24,6 +35,7 @@ export const projects = [
     projectDescription:
       "My React e-commerce site uses the SearchSpring API to let users search for products and view them in a paginated format. The site is fast and responsive, even with large data, and has a clean interface for easy navigation and purchases. SearchSpring API integration delivers accurate and relevant search results, making it a powerful online shopping tool.",
     sourceCodeLink: "https://github.com/conradhunter/mock-e-commerce",
+    workInProgress: false,
     liveLink: "https://mock-ecommerce-searchspringapi.netlify.app/",
     projectImage: require("../assets/ecommerce-ss.png"),
   },
@@ -33,6 +45,7 @@ export const projects = [
     projectDescription:
       "My password generator lets users create secure passwords by selecting the length (4-60) and including uppercase letters, symbols, and numbers. Users can generate unique passwords that are difficult to crack, providing them with the peace of mind they need to keep their personal information safe.",
     sourceCodeLink: "https://github.com/conradhunter/Password-Generator",
+    workInProgress: false,
     liveLink: "https://conradhunter.github.io/Password-Generator/",
     projectImage: require("../assets/password-generator-ss.png"),
   },
